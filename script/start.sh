a2d7cdd9b999d2c010057dae80d2c31ef1d9823e
Rudi Antoni <rudiantoni2013@gmail.com>
2023-01-12 03:52:47 -0300

    Working application

--- script/start.sh
+++ script/start.sh
@@ -4,5 +4,5 @@ CUR_DIR=$(pwd)
 cd docker
 echo
 echo "Starting using the environment file $ENV_FILE..."
-sudo docker-compose --env-file ../$ENV_FILE up
+sudo docker-compose --env-file ../$ENV_FILE up -d
 cd $CUR_DIR
